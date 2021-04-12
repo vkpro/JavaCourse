da2c8efa24bfe915c96d465cabdacf59bc17e327
Denis Tsyganov <dtsyganov@luxoft.com>
2021-04-12 12:42:47 +0600

    1. System.arraycopy -> Arrays.copyOf 2. For loop -> System.arraycopy 3. Константам добавил static

--- src/ru/luxoft/cources/lab7core/MyArrayList.java
+++ src/ru/luxoft/cources/lab7core/MyArrayList.java
@@ -1,17 +1,32 @@
 package ru.luxoft.cources.lab7core;
 
-import javax.sound.midi.Soundbank;
+import java.util.Arrays;
 
 public class MyArrayList<T> {
-    private final int INIT_SIZE = 16;
-    private final int CUT_RATE = 4;
+    private static final int INIT_SIZE = 16;
+    private static final int CUT_RATE = 4;
     private Object[] array = new Object[INIT_SIZE];
     private int pointer = 0;
 
+    public static void main(String[] args) {
+        MyArrayList<String> myList = new MyArrayList<>();
+        System.out.println("Add some elements to MyArrayList...");
+        myList.add("a");
+        myList.add("b");
+        myList.add("c");
+        System.out.println("Get elements from MyArrayList...");
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println(myList.get(i));
+        }
+        System.out.println("Remove element 1 from MyArrayList...");
+        myList.remove(1);
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println(myList.get(i));
+        }
+    }
+
     private void resize(int newLength) {
-        Object[] newArray = new Object[newLength];
-        System.arraycopy(array, 0, newArray, 0, pointer);
-        array = newArray;
+        array = Arrays.copyOf(array, newLength);
     }
 
     public void add(T item) {
@@ -29,29 +44,11 @@ public class MyArrayList<T> {
     }
 
     public void remove(int index) {
-        for (int i = index; i < pointer; i++)
-            array[i] = array[i + 1];
+        System.arraycopy(array, index + 1, array, index, pointer - index - 1);
         array[pointer] = null;
         pointer--;
         if (array.length > INIT_SIZE && pointer < array.length / CUT_RATE)
             resize(array.length / 2); // если элементов в CUT_RATE раз меньше чем
         // длина массива, то уменьшу в два раза
-    }
-
-    public static void main(String[] args) {
-        MyArrayList<String> myList = new MyArrayList<>();
-        System.out.println("Add some elements to MyArrayList...");
-        myList.add("a");
-        myList.add("b");
-        myList.add("c");
-        System.out.println("Get elements from MyArrayList...");
-        for (int i = 0; i < myList.size(); i++) {
-            System.out.println(myList.get(i));
-        }
-        System.out.println("Remove element 1 from MyArrayList...");
-        myList.remove(1);
-        for (int i = 0; i < myList.size(); i++) {
-            System.out.println(myList.get(i));
-        }
     }
 }
