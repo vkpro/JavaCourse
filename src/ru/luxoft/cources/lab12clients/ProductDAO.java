25d2cfe22fbaa470f5a7d5cd21bc8ddbb9c1b3cf
Denis Tsyganov <dtsyganov@luxoft.com>
2021-04-30 17:41:22 +0600

    Lab 12 - ok. небольшой рефакторинг

--- src/ru/luxoft/cources/lab12clients/ProductDAO.java
+++ src/ru/luxoft/cources/lab12clients/ProductDAO.java
@@ -1,97 +1,97 @@
 package ru.luxoft.cources.lab12clients;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDAO {
-    private Connection getConnection() throws Exception {
-        Connection connection = null;
-        String host="localhost";
-        String port="5432";
-        String db_name="testdb";
-        String username="postgres";
-        String password="pass";
-
-        Class.forName("org.postgresql.Driver");
-        connection = DriverManager.getConnection("jdbc:postgresql://"+host+":"+port+"/"+db_name+"",
-                ""+username+"", ""+password+"");
-
-        return connection;
-    }
-
-    public List<Product> getProductById(int id) throws Exception {
-        List<Product> products = new ArrayList<>();
-        Connection connection = getConnection();
-
-        PreparedStatement st = connection.prepareStatement(
-                "SELECT description, rate, quantity " +
-                        "FROM product " +
-                        "WHERE id = ?");
-        st.setInt(1, id);
-
-        ResultSet rs = st.executeQuery();
-
-        Product product = null;
-        while (rs.next()) {
-            product = new Product(
-                    id,
-                    rs.getString(1),
-                    rs.getFloat(2),
-                    rs.getInt(3));
-            products.add(product);
+    private Connection getConnection() {
+        var host = "localhost";
+        var port = "5432";
+        var dbName = "testdb";
+        var username = "postgres";
+        var password = "pass";
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://%s:%s/%s".formatted(host, port, dbName);
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new ConnectionException(ex);
         }
-        rs.close();
-        connection.close();
-        return products;
     }
 
-    public void addProduct(Product product) throws Exception {
-        Connection connection = getConnection();
+    public List<Product> getProductById(int id) throws SQLException {
+        List<Product> products = new ArrayList<>();
+        try (
+                var connection = getConnection();
+                PreparedStatement st = connection.prepareStatement(
+                        "SELECT description, rate, quantity " +
+                                "FROM product " +
+                                "WHERE id = ?")
+        ) {
+            st.setInt(1, id);
 
-        PreparedStatement st = connection.prepareStatement(
-                "INSERT into product " +
-                        "(id, description, rate, quantity) " +
-                        "values (?, ?, ?, ?)");
-        st.setInt(1, product.getId());
-        st.setString(2, product.getDescription());
-        st.setFloat(3, product.getRate());
-        st.setInt(4, product.getQuantity());
+            ResultSet rs = st.executeQuery();
 
-        st.executeUpdate();
-
-        connection.close();
+            while (rs.next()) {
+                Product product = new Product(
+                        id,
+                        rs.getString(1),
+                        rs.getFloat(2),
+                        rs.getInt(3));
+                products.add(product);
+            }
+            rs.close();
+            return products;
+        }
     }
 
-    public void setProduct(Product product) throws Exception {
-        Connection connection = getConnection();
+    public void addProduct(Product product) throws SQLException {
+        try (var connection = getConnection();
+             PreparedStatement st = connection.prepareStatement(
+                     "INSERT into product " +
+                             "(id, description, rate, quantity) " +
+                             "values (?, ?, ?, ?)")
+        ) {
+            st.setInt(1, product.getId());
+            st.setString(2, product.getDescription());
+            st.setFloat(3, product.getRate());
+            st.setInt(4, product.getQuantity());
 
-        PreparedStatement st = connection.prepareStatement(
-                "UPDATE product " +
-                        "SET description=?, rate=?, quantity=? " +
-                        "WHERE id=?");
-        st.setString(1, product.getDescription());
-        st.setFloat(2, product.getRate());
-        st.setInt(3, product.getQuantity());
-        st.setInt(4, product.getId());
-
-        st.executeUpdate();
-        connection.close();
+            st.executeUpdate();
+        }
     }
 
-    public void removeProduct(int id) throws Exception {
-        Connection connection = getConnection();
+    public void setProduct(Product product) throws SQLException {
+        try (var connection = getConnection();
+             PreparedStatement st = connection.prepareStatement(
+                     "UPDATE product " +
+                             "SET description=?, rate=?, quantity=? " +
+                             "WHERE id=?")
+        ) {
+            st.setString(1, product.getDescription());
+            st.setFloat(2, product.getRate());
+            st.setInt(3, product.getQuantity());
+            st.setInt(4, product.getId());
 
-        PreparedStatement st = connection.prepareStatement(
-                "DELETE from product " +
-                        "WHERE id = ?");
-        st.setInt(1, id);
-
-        st.executeUpdate();
-        connection.close();
+            st.executeUpdate();
+        }
     }
 
+    public void removeProduct(int id) throws SQLException {
+        try (var connection = getConnection();
+             PreparedStatement st = connection.prepareStatement(
+                     "DELETE from product WHERE id = ?")
+        ) {
+            st.setInt(1, id);
+
+            st.executeUpdate();
+        }
+    }
+
+    private static class ConnectionException extends RuntimeException {
+        public ConnectionException(Throwable ex) {
+            super(ex);
+        }
+    }
 }
